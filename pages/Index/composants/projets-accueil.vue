e80de8430ee59614d794ccdc50b7d4a3990bec96
Alexis <e1577094@cmaisonneuve.qc.ca>
2020-11-17 17:29:58 -0500

    Router-link sur les liens

--- pages/Index/composants/projets-accueil.vue
+++ pages/Index/composants/projets-accueil.vue
@@ -1,53 +1,55 @@
 <template>
-   <section id="voirProjets">
-        <div>
-            <h2>Web</h2>
-            <img src="medias/images/web.png" alt="Web">
-        </div>
-        <div>
-            <h2>Jeu</h2>
-            <img src="medias/images/jeu.png" alt="Jeu">
-        </div>
-        <div>
-            <h2>3D</h2>
-            <img src="medias/images/3d.png" alt="3D">
-        </div>
-        <button><a href="/pages/projets.html"><h3>Voir Projets</h3></a></button>
-    </section>
+  <section id="voirProjets">
+    <div>
+      <h2>Web</h2>
+      <img src="medias/images/web.png" alt="Web" />
+    </div>
+    <div>
+      <h2>Jeu</h2>
+      <img src="medias/images/jeu.png" alt="Jeu" />
+    </div>
+    <div>
+      <h2>3D</h2>
+      <img src="medias/images/3d.png" alt="3D" />
+    </div>
+    <router-link to="/projets">
+      <button>
+        <a href="/pages/projets.html"><h3>Voir Projets</h3></a>
+      </button>
+    </router-link>
+  </section>
 </template>
 
 <script>
-module.exports ={
-
-}
+module.exports = {};
 </script>
 
 <style>
-    #voirProjets {
-      display: flex;
-      justify-content: space-around;
-      color: white;
-    }
-    
-    #voirProjets div{
-        align-items: center;
-    }
+#voirProjets {
+  display: flex;
+  justify-content: space-around;
+  color: white;
+}
 
-    #voirProjets div h2 {
-        color: white;
-    }
+#voirProjets div {
+  align-items: center;
+}
 
-    #voirProjets button {
-        padding: 0.5em;
-        background-color: #2cd9ff;
-        box-shadow: #1c2c5c;
-    }
+#voirProjets div h2 {
+  color: white;
+}
 
-    #voirProjets button h3 {
-        color: #1c2c5c;
-    }
+#voirProjets button {
+  padding: 0.5em;
+  background-color: #2cd9ff;
+  box-shadow: #1c2c5c;
+}
 
-    #voirProjets button a:hover {
-        color: #2cd9ff;
-    }
+#voirProjets button h3 {
+  color: #1c2c5c;
+}
+
+#voirProjets button a:hover {
+  color: #2cd9ff;
+}
 </style>

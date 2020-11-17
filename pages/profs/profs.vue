330265f7c8c970e70f5eb535cba081a31fdec089
Sabrina Ales <sabrina-ales@hotmail.com>
2020-11-17 18:48:21 -0500

    composant commun dans toutes les pages

--- pages/profs/profs.vue
+++ pages/profs/profs.vue
@@ -1,16 +1,24 @@
 <template>
-  <h1>les profs</h1>
+  <div>
+    <header-commun></header-commun>
+    <footer-commun></footer-commun>
+  </div>
 </template>
+
 
 <script>
 module.exports = {
-
-}
+  // name: 'profs',
+  components: {
+    "header-commun": httpVueLoader("/pages/Index/composants/header-commun.vue"),
+    "footer-commun": httpVueLoader("/pages/commun/footer-commun.vue"),
+  },
+};
 </script>
 
 <style scoped>
-    h1 {
+/* h1 {
         background-color: white;
         color: brown;
-    }
+    } */
 </style>
--- pages/projets/projets.vue
+++ pages/projets/projets.vue
@@ -1,16 +1,24 @@
 <template>
-  <h1>les projets</h1>
+  <div>
+    <h1>les projets</h1>
+    <header-commun></header-commun>
+    <footer-commun></footer-commun>
+  </div>
 </template>
 
 <script>
 module.exports = {
-
-}
+  // name: 'projets',
+  components: {
+    "header-commun": httpVueLoader("/pages/Index/composants/header-commun.vue"),
+    "footer-commun": httpVueLoader("/pages/commun/footer-commun.vue"),
+  },
+};
 </script>
 
 <style scoped>
-    h1 {
-        background-color: white;
-        color: brown;
-    }
+h1 {
+  background-color: white;
+  color: brown;
+}
 </style>

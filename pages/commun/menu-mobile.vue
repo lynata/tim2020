0086332e7ef8ec1f0b1b93fe34f988f00b23c589
Sabrina Ales <sabrina-ales@hotmail.com>
2020-11-16 21:18:57 -0500

    menu mobile

---
+++ pages/commun/menu-mobile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>je suis dans le menu</h1>
+    <h2>aldowdlad</h2>
+    dwodawdawd
+  </div>
+</template>
+
+
+
+<style scoped>
+div {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: green;
+  z-index: 99;
+  height: 100%;
+  width: 100vw;
+}
+</style>

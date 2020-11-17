8ede33dfee8b0c3d177772742f090034ea7cfae3
Lyna <lyly8888@hotmail.com>
2020-11-17 17:25:33 -0500

    avancement voirProjet

--- pages/Index/composants/projets-accueil.vue
+++ pages/Index/composants/projets-accueil.vue
@@ -22,32 +22,44 @@ module.exports ={
 }
 </script>
 
-<style>
+<style scoped>
+
+    #voirProjets div h2 {
+        position: absolute;
+        color: white;
+        font-family: CastIron;
+        font-size: 5rem;
+        margin: -2% -1%;
+        padding: 0.5% 2%;
+        background-color: #2cd9ff;
+        /* z-index: 0; */
+    }
+
     #voirProjets {
-      display: flex;
-      justify-content: space-around;
-      color: white;
+        padding-top: 6%;
+        display: flex;
+        justify-content: space-around;
     }
     
     #voirProjets div{
-        align-items: center;
-    }
-
-    #voirProjets div h2 {
-        color: white;
+        display: flex;
+        flex-direction: row-reverse;
     }
 
     #voirProjets button {
-        padding: 0.5em;
+        border: red 1px solid;
+        padding: 0 2rem;
         background-color: #2cd9ff;
         box-shadow: #1c2c5c;
     }
 
     #voirProjets button h3 {
+        font-family: CastIron;
+        font-size: 2rem;
         color: #1c2c5c;
     }
 
-    #voirProjets button a:hover {
+    #voirProjets button:hover {
         color: #2cd9ff;
     }
 </style>

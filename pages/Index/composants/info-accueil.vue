2ab5a2e5e91cc019ff8db3d5244d34de96f66666
Sabrina Ales <sabrina-ales@hotmail.com>
2020-11-18 19:44:22 -0500

    text boutons info

--- pages/Index/composants/info-accueil.vue
+++ pages/Index/composants/info-accueil.vue
@@ -1,23 +1,5 @@
 <template>
   <section id="info">
-    <!-- <div id="infoBloc">
-      <h1>Technique d'intÉgration multimÉdia</h1>
-      <p>
-        Maisonneuve est le seul cégep sur l’Île de Montréal à offrir ce
-        programme qui vous offre une immersion dans le monde du jeu 2D/3D, du
-        Web, des technologies mobiles et des médias numériques, tout en
-        apprenant différents langages de programmation. Dans ce programme, vous
-        apprendrez à identifier, analyser, imaginer, réaliser et coordonner un
-        projet multimédia et les différentes étapes de production qui y sont
-        liées :
-      </p>
-      <div id="infoBoutons">
-        <button>Conception</button>
-        <button>Mediatisation</button>
-        <button>Programmation</button>
-        <button>Integration</button>
-      </div>
-    </div> -->
     <div id="infoBloc">
       <!-- boutons-->
       <div
@@ -49,11 +31,12 @@
         </li>
         <li>
           <h1>Mediatisation</h1>
-          La Médiatisation est la création de médias multimédias.
+          Création de médias de tout formes: text, images, animation, et plus
+          encore!
         </li>
         <li>
           <h1>Integration</h1>
-          L'intégration est l'intégration de trucs intégrables.
+          Intègre les éléments des pages d’un site.
         </li>
       </ul>
     </div>

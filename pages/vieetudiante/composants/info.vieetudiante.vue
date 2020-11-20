b5c95dd9449696161bbe0aaa90f7d6f4621fceb0
Sabrina Ales <sabrina-ales@hotmail.com>
2020-11-19 20:44:34 -0500

    vie etudiante fix et ajouts

---
+++ pages/vieetudiante/composants/info.vieetudiante.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="/medias/images/timscuits.PNG" />
+    <img src="/medias/images/elevesEcoutent.PNG" />
+    <img src="/medias/images/elevesEcoutent2.PNG" />
+  </div>
+</template>
+
+<script>
+module.exports = {};
+</script>
+
+<style scoped>
+@media screen and (min-width: 600px) {
+}
+@media screen and (min-width: 1200px) {
+}
+</style>

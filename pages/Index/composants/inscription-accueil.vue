31292e73da0065311265b3501bddc4fac7cc6934
Claudia Patry <71995934+Claue-roe@users.noreply.github.com>
2020-11-22 22:37:07 -0500

    Ajout transition et ajustements section inscription

--- pages/Index/composants/inscription-accueil.vue
+++ pages/Index/composants/inscription-accueil.vue
@@ -1,21 +1,18 @@
 <template>
   <section id="inscription">
-    <h2>Inscription</h2>
+    <h2 id="TitreInscription">Inscription</h2>
     <!-- <img src="medias/images/inscription.png" alt="Inscription" /> -->
-    <div>
+    <div id="boutonsInscription">
       <a href="https://admission.sram.qc.ca/" class="boutonArrondi"
-        >Je suis sûr-e!</a
-      >
+        >Je suis sûr-e!</a>
       <a
         href="https://www.cmaisonneuve.qc.ca/accueil/venez-rencontrer/etudiant-dun-jour/"
         class="boutonArrondi"
-        >Étudiant d'un jour</a
-      >
+        >Étudiant d'un jour</a>
       <a
         href="https://www.cmaisonneuve.qc.ca/vie-etudiante/orientation-emploi/choix-de-carriere-orientation/test-dorientation/"
         class="boutonArrondi"
-        >Test d'orientation</a
-      >
+        >Test d'orientation</a>
     </div>
     <!-- ajouter target="_blank" dans la balise a pour ouvrir le lien sur une autre page -->
   </section>
@@ -25,67 +22,77 @@
 module.exports = {};
 </script>
 
-<style scoped lang="scss">
+<style  lang="scss" scoped>
+/*MOBILE FIRST-----------------------------------------------------*/
 #inscription {
-  margin: 0;
+  width: 100%;
+  height: 50%;
   margin: 5% 0;
   display: block;
   background-image: url("/../medias/images/inscription.png");
   background-repeat: no-repeat;
-  background-position: 80%;
+  background-position: 95%;
+  background-size: 80%;
 
-  h2 {
-    font-family: CastIron;
+  #TitreInscription {
+    font-family: "CastIron";
     color: white;
-    font-size: 5em;
+    font-size: 2em;
     margin-left: 5%;
   }
 
-  //   img {
-  //     position: absolute;
-  //     right: 0;
-  //     padding-bottom: 5%;
-  //     // border: red 1px solid;
-  //   }
+   .boutonArrondi { 
+      margin:0.5em;
+      padding: 0.9em;
+      font-size: 0.5em;
+    }
+}
 
-  div {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    a {
-      font-family: ManRope;
-      margin-left: 5%;
-      background-color: #101832;
-      border: #2cd9ff 6px solid;
-      // border-radius: 50px;
-      // padding: 10px;
-      display: block;
-      // font-size: 1em;
-      // z-index: 2;
-      width: 20%;
-    }
-    a:nth-child(1):hover {
-      width: 30%;
-      margin-bottom: 1%;
-    }
-    a:nth-child(2):hover {
-      width: 40%;
-      margin-bottom: 1%;
-    }
-    a:nth-child(3):hover {
-      width: 50%;
-    }
 
-    a:hover {
-      background-color: #2cd9ff;
-      color: black;
-    }
+/*DEBUT DESKTOP-----------------------------------------------------*/
+@media (min-width: 1200px) {
+
+  #TitreInscription {
+    font-size: 5em !important;
+  }
+
+#boutonsInscription {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin-left: 5%;
+  .boutonArrondi { 
+    font-size: 0.8em !important;
+    display: block;
+  }
+
+  .boutonArrondi:nth-child(1) {
+    transition: width 0.2s ease-out;
+    width: 14%;
+  }
+  .boutonArrondi:nth-child(2) {
+    transition: width 0.2s ease-out;
+    width: 28%;
+  }
+  .boutonArrondi:nth-child(3) {
+    transition: width 0.2s ease-out;
+    width: 20%;
+  }
+  
+  .boutonArrondi:nth-child(1):hover {
+    transition: width 0.2s ease-in;
+    width: 18%;
+  }
+  .boutonArrondi:nth-child(2):hover {
+    transition: width 0.2s ease-in;
+    width: 32%;
+  }
+  .boutonArrondi:nth-child(3):hover {
+    transition: width 0.2s ease-in;
+    width: 25%;
   }
 }
 
-// @media (min-width: 600px){
-//     a{
-        
-//     }
-// }
+}
+
 </style>

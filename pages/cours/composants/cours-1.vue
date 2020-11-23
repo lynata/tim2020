38472245ec6d4e5958e5fda5ef6815402e264341
Sabrina Ales <sabrina-ales@hotmail.com>
2020-11-23 00:22:51 -0500

    Cours 1-4 debut css

--- pages/cours/composants/cours-1.vue
+++ pages/cours/composants/cours-1.vue
@@ -1,8 +1,20 @@
 <template>
   <section class="coursPopup">
     <div class="descCours">
-      <h2>ceci est un cours</h2>
-      <p>ceci est une courte description du cours</p>
+      <p>
+        Ce cours initie l’étudiant au traitement des images en mouvement, au
+        traitement du son ainsi qu'aux bases de la scénarisation linéaire.
+        L’étudiant apprend à utiliser les périphériques associés au traitement
+        du son et de la vidéo et à exploiter de façon créative les fonctions des
+        logiciels de traitement vidéo et sonore. Les techniques de montage
+        (structure narrative, affinage de coupe, correction de couleur, etc.)
+        sont étudiées. L’étudiant apprend à représenter ses idées et ses
+        concepts par le dessin et l’écrit, à l’aide de scénarimages, de synopsis
+        et de scénarios. Les particularités de la fonction de travail liées au
+        traitement de la vidéo, du son, ainsi qu'à la scénarisation sont
+        présentées de même que les compétences techniques et comportementales
+        associées.
+      </p>
     </div>
     <div class="descCours">
       <h3>enseignants</h3>
---
+++ pages/cours/composants/cours-4.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="coursPopup">
+    <div class="descCours">
+      <p>
+        Ce cours est consacré aux concepts de base nécessaires à la création
+        d’un jeu numérique. Le cours porte, d’une part, sur l’animation des
+        médias : l’étudiant y apprend à importer et à intégrer des médias dans
+        un environnement de création de jeu. De plus, il se familiarise avec les
+        techniques de création d’animations à partir d’images simples et des
+        feuilles de « sprites ». D’autre part, le cours couvre la gestion des
+        interactions : l’étudiant y est initié aux concepts de base de
+        l’interactivité dans un contexte de jeu, ainsi qu’aux techniques de
+        conception, de codification et de vérification des algorithmes
+        nécessaires à cette fin. Dans ce cours, l’étudiant est également initié
+        à l’environnement matériel et technologique dans lequel il évoluera tout
+        au long de sa formation et de sa carrière. Les particularités de la
+        fonction de travail liées au domaine du jeu sont présentées, de même que
+        les compétences techniques et comportementales associées.
+      </p>
+    </div>
+    <div class="descCours">
+      <h3>enseignants</h3>
+      <p>liste prof</p>
+      <p>qui enseigne ce cours</p>
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+.coursPopup {
+  position: absolute;
+  background-color: #fff;
+  h2 {
+    color: #000;
+  }
+}
+</style>

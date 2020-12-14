c53884c484e66846e20489de3325ccb34ccb94ef
Claudia Patry <71995934+Claue-roe@users.noreply.github.com>
2020-12-14 07:36:10 -0500

    Ajout sections vie étudiante

---
+++ pages/vieetudiante/composants/centre-aide-vieetudiante.vue
@@ -0,0 +1,45 @@
+<template>
+  <section id="centre-aide-ve">
+      <h1 id="titre-centre-aide">Centre d'aide</h1>
+      <img src="/medias/images/GregExplique.PNG" alt="Élève est aidé par un professeur">
+      <p id="desc-centre-aide">Besoin d’aide pour vos projets? Des étudiants experts dans leur domaine sont ici pour vous aider. Leur tutorat est disponible tout les mardis et les jeudis dans le local B1152. Venez nous voir!</p>
+  </section>
+</template>
+
+<script lang="text/babel">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$fond: #090c16;
+$bleuFond: #1c2c5c;
+$blanc: #e3e3e3;
+$bleu: #2cd9ff;
+
+/* MOBILE FIRST--------------------------------------------------------------------------------------------- */
+#centre-aide-ve{
+  background-color: $fond;
+  width: 100%;
+  height:100%;
+  
+}
+
+#titre-centre-aide{
+  font-family: "CastIron";
+  color: $bleu;
+}
+
+
+img{
+    width: 35em;
+    height: 25em;
+    object-fit: cover;
+    margin: 0;
+}
+
+
+/* VERSION DESKTOP------------------------------------------------------------------------------------------ */
+@media (min-width: 1200px) {
+
+}
+</style>
---
+++ pages/vieetudiante/composants/galerie-vieetudiante.vue
@@ -0,0 +1,59 @@
+<template>
+  <section id="galerie-ve">
+    <h1 id="titre-galerie">Galerie</h1>
+    <h2 id="annee-galerie"></h2>
+    <div id="imgs-galerie">
+      <img src="/medias/images/timscuits.PNG" />
+      <img src="/medias/images/elevesEcoutent.PNG" />
+      <img src="/medias/images/elevesEcoutent2.PNG" />
+      <img src="/medias/images/equipeProf2017 retouch.jpg" />
+      <img src="/medias/images/EleveClasse.PNG" />
+      <img src="/medias/images/ludo.PNG" />
+      <img src="/medias/images/fete.PNG" />
+      <img src="/medias/images/Manette.PNG" />
+    </div>
+  </section>
+</template>
+
+<script lang="text/babel">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$fond: #090c16;
+$bleuFond: #1c2c5c;
+$blanc: #e3e3e3;
+$bleu: #2cd9ff;
+
+/* MOBILE FIRST--------------------------------------------------------------------------------------------- */
+#galerie-ve{
+  background-color: $fond;
+  width: 100%;
+  height:100%;
+}
+
+#titre-galerie{
+  font-family: "CastIron";
+  color: $bleu;
+}
+
+#imgs-galerie {
+    img{
+        width: 35em;
+        height: 25em;
+        object-fit: cover;
+        margin: 0;
+    }
+}
+
+/* VERSION DESKTOP------------------------------------------------------------------------------------------ */
+@media (min-width: 1200px) {
+
+#imgs-galerie {
+    img{
+        float: center;
+    }
+}
+
+}
+</style>
